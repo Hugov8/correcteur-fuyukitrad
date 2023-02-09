ba24a98148c139f9a9fdf870528e032aab246cc4
Hugo <hugovedrine@hotmail.com>
2023-02-09 15:04:40 +0100

    test check code

--- front/index.js
+++ front/index.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit.rateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.get('/', function (req, res) {
   let index = path.join(__dirname, 'build', 'index.html') 
   if (isValidPath(index)) {
