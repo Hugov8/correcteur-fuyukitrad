4bd3d0c01dab70048552779e280999e19bd9682c
Hugo <hugovedrine@hotmail.com>
2023-02-12 14:22:46 +0100

    test cd

--- decrypt.sh
+++ decrypt.sh
@@ -3,4 +3,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output backend/correction-sheets/client_secret.json backend/correction-sheets/client_secret.json.gpg
+--output $HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json \
+$HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json.gpg
