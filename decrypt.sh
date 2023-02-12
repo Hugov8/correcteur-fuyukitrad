d961e35adfb95d7b69211260b67104b045c29ae4
Hugo <hugovedrine@hotmail.com>
2023-02-12 15:29:13 +0100

    fix access create file3

--- decrypt.sh
+++ decrypt.sh
@@ -2,7 +2,7 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-chmod +x $HOME/correcteur-fuyukitrad/backend/correction-sheets
+chmod +w $HOME/correcteur-fuyukitrad/backend/correction-sheets
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json \
 $HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json.gpg
