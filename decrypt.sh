f509df41f863291228a27789ca32e1a65eeceb3e
Hugo <hugovedrine@hotmail.com>
2023-02-12 16:12:20 +0100

    supprimer ligne inutile

--- decrypt.sh
+++ decrypt.sh
@@ -2,7 +2,7 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-chmod +w $HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json
+
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json \
 $HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json.gpg
