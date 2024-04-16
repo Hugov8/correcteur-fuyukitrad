567daec99b710281cda0d2d5682cbb858ad7e77f
Hugo V <hugovedrine@hotmail.com>
2024-04-16 18:34:27 +0200

    Suppression client secret.json

--- decrypt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json \
-$HOME/correcteur-fuyukitrad/backend/correction-sheets/client_secret.json.gpg
