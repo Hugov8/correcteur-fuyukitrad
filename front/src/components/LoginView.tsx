2f3ae729ea29114dfe743407c4fa10b0f4160fa5
Hugo V <hugovedrine@hotmail.com>
2026-02-04 23:49:21 +0100

    Fix affichage date et nginx conf

--- front/src/components/LoginView.tsx
+++ front/src/components/LoginView.tsx
@@ -2,6 +2,16 @@ import { URL_LOGIN, URL_LOGOUT } from "../calls/authService"
 import { StringOrNull } from "../calls/commonType"
 import { Boutton } from "./Boutton"
 
+const formatLocalTime = (iso: StringOrNull) => {
+  if (!iso) return "";
+  return new Date(iso).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false
+  });
+};
+
+
 export const LoginView = ({user, expires}: {user: StringOrNull, expires: StringOrNull}) => {
     const login = () => {
         window.location.replace(URL_LOGIN)
@@ -13,9 +23,9 @@ export const LoginView = ({user, expires}: {user: StringOrNull, expires: StringO
     if(user == null) return <div style={{display: "flex", gap: "1px", justifyContent: "flex-end", padding: "0 16px"}}>
             <Boutton text="Se connecter" onClick={login}/>
         </div>
-
+    const expiresDate = formatLocalTime(expires)
     return <div style={{display: "flex", gap: "1px", justifyContent: "flex-end", padding: "0 16px"}}>
-        <p>Connecté en tant que : {user} jusque {expires}</p>
+        <p>Connecté en tant que : {user} jusqu'à {expiresDate}</p>
         <Boutton text="Se déconnecter" onClick={logout} />
     </div>
 }
